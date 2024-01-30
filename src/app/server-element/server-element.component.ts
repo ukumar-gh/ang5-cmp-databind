eade8fd466c8edf079ba718798401d005275a14a
Kumar MA <public.kumar@gmail.com>
2024-01-30 09:18:17 -0800

    Fixed compile error

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrl: './server-element.component.css'
+  styleUrl: './server-element.component.css',
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent {
   @Input('srvElement') element: {type: string, name: string, content: string};
